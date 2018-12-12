4a8d30e2798e628a8c0ba32ce4e2a79822940444
Andrej Ivanov <ivandrej@gmail.com>
2018-12-12 23:55:18 +0100

    Intermediate vanilla gan

--- gan.py
+++ gan.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-# Collect the dataset e
-mnist = input_data.read_data_sets("MNIST_data/")
-
 
 def discriminator(x_image, reuse=False):
 
@@ -70,15 +67,181 @@ def discriminator(x_image, reuse=False):
 
     return d4
 
+
 def generator(batch_size, z_dim):
-    z = tf.truncated_normal([batch_size, z_dim], mean=0, sttdev=1.0, name='z')
+    # Collect the dataset e
+    mnist = input_data.read_data_sets("MNIST_data/")
+
+    z = tf.truncated_normal([batch_size, z_dim], mean=0, stddev=1, name='z')
+    # first deconv block
+    g_w1 = tf.get_variable('g_w1', [z_dim, 3136], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b1 = tf.get_variable('g_b1', [3136], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g1 = tf.matmul(z, g_w1) + g_b1
+    g1 = tf.reshape(g1, [-1, 56, 56, 1])
+    g1 = tf.contrib.layers.batch_norm(g1, epsilon=1e-5, scope='bn1')
+    g1 = tf.nn.relu(g1)
+
+    # Generate 50 features
+    g_w2 = tf.get_variable('g_w2', [3, 3, 1, z_dim/2], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b2 = tf.get_variable('g_b2', [z_dim/2], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g2 = tf.nn.conv2d(g1, g_w2, strides=[1, 2, 2, 1], padding='SAME')
+    g2 = g2 + g_b2
+    g2 = tf.contrib.layers.batch_norm(g2, epsilon=1e-5, scope='bn2')
+    g2 = tf.nn.relu(g2)
+    g2 = tf.image.resize_images(g2, [56, 56])
+
+    # Generate 25 features
+    g_w3 = tf.get_variable('g_w3', [3, 3, z_dim/2, z_dim/4], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b3 = tf.get_variable('g_b3', [z_dim/4], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g3 = tf.nn.conv2d(g2, g_w3, strides=[1, 2, 2, 1], padding='SAME')
+    g3 = g3 + g_b3
+    g3 = tf.contrib.layers.batch_norm(g3, epsilon=1e-5, scope='bn3')
+    g3 = tf.nn.relu(g3)
+    g3 = tf.image.resize_images(g3, [56, 56])
+
+    # Final convolution with one output channel
+    g_w4 = tf.get_variable('g_w4', [1, 1, z_dim/4, 1], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b4 = tf.get_variable('g_b4', [1], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g4 = tf.nn.conv2d(g3, g_w4, strides=[1, 2, 2, 1], padding='SAME')
+    g4 = g4 + g_b4
+    g4 = tf.sigmoid(g4)
+
+    # No batch normalization at the final layer, but we do add
+    # a sigmoid activator to make the generated images crisper.
+    # Dimensions of g4: batch_size x 28 x 28 x 1
+
+    return g4
+
 
 if __name__ == "__main__":
+    # Collect the dataset
+    mnist = input_data.read_data_sets("MNIST_data/")
+
     sess = tf.Session()
 
     batch_size = 50
     z_dimensions = 100
 
-    x_placeholder = tf.placeholder('float', shape=[None, 28, 28, 1], name='placeholder')
+    x_placeholder = tf.placeholder("float", shape = [None,28,28,1], name='x_placeholder')
 
     Gz = generator(batch_size, z_dimensions)
+
+    Dx = discriminator(x_placeholder)
+
+    Dg = discriminator(Gz, reuse=True)
+
+    g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, labels=tf.ones_like(Dg)))
+
+    d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dx, labels=tf.fill([batch_size, 1], 0.9)))
+    d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, labels=tf.zeros_like(Dg)))
+    d_loss = d_loss_real + d_loss_fake
+
+    tvars = tf.trainable_variables()
+
+    d_vars = [var for var in tvars if 'd_' in var.name]
+    g_vars = [var for var in tvars if 'g_' in var.name]
+
+    with tf.variable_scope(tf.get_variable_scope(), reuse=False) as scope:
+        d_trainer_fake = tf.train.AdamOptimizer(0.0001).minimize(d_loss_fake, var_list=d_vars)
+        d_trainer_real = tf.train.AdamOptimizer(0.0001).minimize(d_loss_real, var_list=d_vars)
+
+        g_trainer = tf.train.AdamOptimizer(0.0001).minimize(g_loss, var_list=g_vars)
+
+    tf.summary.scalar('Generator_loss', g_loss)
+    tf.summary.scalar('Discriminator_loss_real', d_loss_real)
+    tf.summary.scalar('Discriminator_loss_fake', d_loss_fake)
+
+    d_real_count_ph = tf.placeholder(tf.float32)
+    d_fake_count_ph = tf.placeholder(tf.float32)
+    g_count_ph = tf.placeholder(tf.float32)
+
+    tf.summary.scalar('d_real_count', d_real_count_ph)
+    tf.summary.scalar('d_fake_count', d_fake_count_ph)
+    tf.summary.scalar('g_count', g_count_ph)
+
+    # Sanity check to see how the discriminator evaluates
+    # generated and real MNIST images
+    d_on_generated = tf.reduce_mean(discriminator(generator(batch_size, z_dimensions)))
+    d_on_real = tf.reduce_mean(discriminator(x_placeholder))
+
+    tf.summary.scalar('d_on_generated_eval', d_on_generated)
+    tf.summary.scalar('d_on_real_eval', d_on_real)
+
+    images_for_tensorboard = generator(batch_size, z_dimensions)
+    tf.summary.image('Generated_images', images_for_tensorboard, 10)
+    merged = tf.summary.merge_all()
+    logdir = "tensorboard/gan/"
+    writer = tf.summary.FileWriter(logdir, sess.graph)
+    print(logdir)
+
+    saver = tf.train.Saver()
+
+    sess.run(tf.global_variables_initializer())
+
+
+    gLoss = 0
+    dLossFake, dLossReal = 1, 1
+    d_real_count, d_fake_count, g_count = 0, 0, 0
+    for i in range(50000):
+        real_image_batch = mnist.train.next_batch(batch_size)[0].reshape([batch_size, 28, 28, 1])
+        if dLossFake > 0.6:
+            # Train discriminator on generated images
+            _, dLossReal, dLossFake, gLoss = sess.run([d_trainer_fake, d_loss_real, d_loss_fake, g_loss],
+                                                      {x_placeholder: real_image_batch})
+            d_fake_count += 1
+
+        if gLoss > 0.5:
+            # Train the generator
+            _, dLossReal, dLossFake, gLoss = sess.run([g_trainer, d_loss_real, d_loss_fake, g_loss],
+                                                      {x_placeholder: real_image_batch})
+            g_count += 1
+
+        if dLossReal > 0.45:
+            # If the discriminator classifies real images as fake,
+            # train discriminator on real values
+            _, dLossReal, dLossFake, gLoss = sess.run([d_trainer_real, d_loss_real, d_loss_fake, g_loss],
+                                                      {x_placeholder: real_image_batch})
+            d_real_count += 1
+
+
+        if i % 10 == 0:
+            real_image_batch = mnist.validation.next_batch(batch_size)[0].reshape([batch_size, 28, 28, 1])
+            summary = sess.run(merged, {x_placeholder: real_image_batch, d_real_count_ph: d_real_count,
+                                        d_fake_count_ph: d_fake_count, g_count_ph: g_count})
+            writer.add_summary(summary, i)
+            d_real_count, d_fake_count, g_count = 0, 0, 0
+
+        if i % 1000 == 0:
+            # Periodically display a sample image in the notebook
+            # (These are also being sent to TensorBoard every 10 iterations)
+            images = sess.run(generator(3, z_dimensions))
+            d_result = sess.run(discriminator(x_placeholder), {x_placeholder: images})
+            print("TRAINING STEP", i, "AT", datetime.datetime.now())
+            for j in range(3):
+                print("Discriminator classification", d_result[j])
+                im = images[j, :, :, 0]
+                plt.imshow(im.reshape([28, 28]), cmap='Greys')
+                plt.show()
+
+        if i % 5000 == 0:
+            save_path = saver.save(sess, "models/pretrained_gan.ckpt", global_step=i)
+            print("saved to %s" % save_path)
+
+    test_images = sess.run(generator(10, 100))
+    test_eval = sess.run(discriminator(x_placeholder), {x_placeholder: test_images})
+
+    real_images = mnist.validation.next_batch(10)[0].reshape([10, 28, 28, 1])
+    real_eval = sess.run(discriminator(x_placeholder), {x_placeholder: real_images})
+
+    # Show discriminator's probabilities for the generated images,
+    # and display the images
+    for i in range(10):
+        print(test_eval[i])
+        plt.imshow(test_images[i, :, :, 0], cmap='Greys')
+        plt.show()
+
+    # Now do the same for real MNIST images
+    for i in range(10):
+        print(real_eval[i])
+        plt.imshow(real_images[i, :, :, 0], cmap='Greys')
+        plt.show()
