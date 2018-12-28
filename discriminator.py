42c12edf2b6bbac6d6fba71d4709340017cd10cd
Andrej Ivanov <ivandrej@gmail.com>
2018-12-28 18:12:14 +0100

    Document entropy findings

--- discriminator.py
+++ discriminator.py
@@ -24,6 +24,7 @@ class Discriminator:
 		model.add(tf.keras.layers.Dropout(0.3))
 		   
 		model.add(tf.keras.layers.Flatten())
+		# Remove sigmoid activation ?????????????
 		model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 	 
 		return model
@@ -38,13 +39,6 @@ class Discriminator:
 	def loss(self, real_output, generated_output):
 		real_loss = tf.losses.sigmoid_cross_entropy( \
 			multi_class_labels=tf.ones_like(real_output), logits=real_output)
-
-		#real_loss1 = tf.reduce_mean(tf.log(tf.reshape(real_output, (real_output.shape[0]))))
-		#print(real_output.shape)
-		#real_loss1 = tf.log(real_output)
-		#real_loss1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=real_output, labels=tf.ones_like(real_output)))
-		#real_loss2 = tf.reduce_mean(tf.log(real_output + 0.00001))
-		#print(real_loss, real_loss1, real_loss2)
 
 		generated_loss = tf.losses.sigmoid_cross_entropy( \
 			multi_class_labels=tf.zeros_like(generated_output), logits=generated_output)
--- mutations.py
+++ mutations.py
@@ -2,4 +2,7 @@ import tensorflow as tf
 tf.enable_eager_execution
 
 def heuristic_mutation(Dgz):
-	return tf.losses.sigmoid_cross_entropy(tf.ones_like(Dgz), Dgz)
+	loss = tf.losses.sigmoid_cross_entropy(tf.ones_like(Dgz), Dgz)
+
+	print("Losses: ", loss) 
+	return loss
