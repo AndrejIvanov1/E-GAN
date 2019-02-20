804900de9b2290f70908fa51d830e633798550d4
Andrej Ivanov <ivandrej@gmail.com>
2019-02-20 20:53:37 +0000

    Working version of optimized EGAN

--- cloud_run_ml_engine.bat
+++ cloud_run_ml_engine.bat
@@ -1,10 +1,10 @@
-gcloud ml-engine jobs submit training fixed_gan_correct  ^
+gcloud ml-engine jobs submit training fixed_gan_correct_gamma_0_4  ^
  	--module-name=trainer.main ^
  	--package-path=trainer ^
- 	--job-dir=gs://gan_datasets/fixed_gan/gamma_0_4 ^
+ 	--job-dir=gs://gan_datasets/fixed_gan/gamma_0_4_secondrun ^
  	--staging-bucket=gs://gan_datasets ^
  	--python-version=3.5 ^
  	--runtime-version=1.12 ^
  	--scale-tier=BASIC_GPU ^
  	--region=europe-west1 ^
- 	-- --network-type EGAN --batch-size 256 --disc-train-steps 2 --epochs 6 --gamma 0.4
+ 	-- --network-type EGAN --batch-size 256 --disc-train-steps 2 --epochs 2 --gamma 0.4
