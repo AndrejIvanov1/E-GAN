e92b654348ee22178de7cc7676744a1fe23675e9
Andrej Ivanov <ivandrej@gmail.com>
2019-02-27 23:41:04 +0000

    Change some defaults

--- cloud_run_ml_engine.bat
+++ cloud_run_ml_engine.bat
@@ -1,10 +1,10 @@
-gcloud ml-engine jobs submit training variable_implementation_3_optimizers_1_z ^
+gcloud ml-engine jobs submit training variable_implementation_1_optimizer_separate_z ^
  	--module-name=trainer.main ^
  	--package-path=trainer ^
- 	--job-dir=gs://gan_datasets/variable_implementation/optimizers_3_same_z ^
+ 	--job-dir=gs://gan_datasets/variable_implementation/1_optimizer_separate_z ^
  	--staging-bucket=gs://gan_datasets ^
  	--python-version=3.5 ^
  	--runtime-version=1.12 ^
  	--scale-tier=BASIC_GPU ^
  	--region=europe-west1 ^
- 	-- --network-type EGAN --batch-size 256 --disc-train-steps 2 --epochs 10 --gamma 0.5
+ 	-- --network-type EGAN --batch-size 256 --disc-train-steps 2 --epochs 10 --gamma 0.3
--- trainer/egan.py
+++ trainer/egan.py
@@ -28,7 +28,7 @@ class EGAN:
 
 		self._mutations = [heuristic_mutation, minimax_mutation, least_square_mutation]
 		#self._optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
-		self._optimizers = [tf.train.AdamOptimizer(learning_rate=1e-3) for mutation in self._mutations]
+		self._optimizers = [tf.train.AdamOptimizer(learning_rate=1e-4) for mutation in self._mutations]
 
 		self._num_examples_to_generate = 16
 		self._random_vector_for_generation = tf.random_normal([self._num_examples_to_generate, noise_dim])
@@ -111,6 +111,7 @@ class EGAN:
 				z = tf.random_normal([self._batch_size, self._noise_dim])
 				losses = self.mutate_children(parent, mutations, tape, z, record_loss)
 
+			z = tf.random_normal([self._batch_size, self._noise_dim])
 			children_values, Gz = self.apply_gradients(tape, parent, losses, z)
 
 			# Can defun
